0aa5d470a1530b07fa6399a15c63ecebd95ca4ba
RepelloInimicum <dualsgo@icloud.com>
2024-06-03 21:54:02 -0300

    Curso Python #03 - Instalando o Python3 e o IDLE

--- cursoemvideo/aula_02.py
+++ cursoemvideo/aula_02.py
@@ -1,10 +1,14 @@
 # Curso Python #02 - Para que serve o Python?
 
-"""De onde vem o Python? Por que esse nome? Quais são as grandes empresas mundiais que usam Python em seus softwares? Essas e muitas outras perguntas serão respondidas nessa aula. A linguagem Python foi criada na Holanda em 1989 pelo programador Guido Van Hossum, que trabalhava no Centro de Matemática e Computação (CWI) em Amsterdam.
+"""De onde vem o Python? Por que esse nome? Quais são as grandes empresas mundiais que usam Python em seus softwares? Essas e muitas outras perguntas serão respondidas nessa aula.
+
+A linguagem Python foi criada na Holanda em 1989 pelo programador Guido Van Hossum, que trabalhava no Centro de Matemática e Computação (CWI) em Amsterdam.
 
 O nome Python foi uma homenagem ao seriado inglês Monty Python Flying Circus, sendo ideia do próprio Van Hossum, já que os projetos dentro do CWI tinham nomes de programas de televisão.
 
-Atualmente a linguagem é mantida por uma fundação sem fins lucrativos chamada Python Software Foundation, apoiada por várias empresas como Microsoft, Google e Globo. Python é uma linguagem multiplataforma interpretada de propósito geral, fácil, intuitiva e principalmente muito organizada. Python é software livre e 100% orientada a objetos. A linguagem Python é muito usada nos campos de inteligência artificial, computação 3D, biotecnologia e realidade virtual, graças a essa simplicidade, versatilidade e capacidade de processamento.
+Atualmente a linguagem é mantida por uma fundação sem fins lucrativos chamada Python Software Foundation, apoiada por várias empresas como Microsoft, Google e Globo. Python é uma linguagem multiplataforma interpretada de propósito geral, fácil, intuitiva e principalmente muito organizada. Python é software livre e 100% orientada a objetos.
+
+A linguagem Python é muito usada nos campos de inteligência artificial, computação 3D, biotecnologia e realidade virtual, graças a essa simplicidade, versatilidade e capacidade de processamento.
 
 Quem usa Python? A Zope, Air Canada, BitTorrent, Globo, Google, YouTube, NASA, Industrial Light & Magic (Python foi usado em Star Wars), Autodesk e muito mais.
 
@@ -17,7 +21,7 @@ A linguagem Python evoluiu ao longo dos anos, tornando-se uma das linguagens pad
 
 A linguagem de programação Python é conhecida por sua simplicidade, versatilidade e organização, sendo adotada em diversos produtos e jogos. Sua filosofia inclui código bonito, simplicidade e leitura clara para facilitar a programação. Python é uma linguagem multiplataforma, funcionando em diversos sistemas e dispositivos, facilitando sua utilização no dia a dia. Python é uma linguagem de programação livre e aberta, permitindo que os usuários baixem, estudem e modifiquem o código conforme necessário. A filosofia do Python valoriza a simplicidade, organização e legibilidade do código, incentivando a prática de programação eficiente e clara.
 
-A linguagem de programação Python é amplamente utilizada em diversas áreas, como biotecnologia, computação 3D e criação de conteúdo, sendo adotada por grandes empresas e na indústria de jogos. Python é essencial na evolução de sistemas de biotecnologia e computação 3D, áreas cruciais para o país. Grandes empresas como Zon, Globo, Air Canada e NASA utilizam Python em seus sistemas e projetos, mostrando sua versatilidade e importância. Além de empresas, a linguagem Python é amplamente empregada na indústria de jogos, com títulos populares como Civilization e Battlefield 2 sendo desenvolvidos com ela.
+A linguagem de programação Python é amplamente utilizada em diversas áreas, como biotecnologia, computação 3D e criação de conteúdo, adotada por grandes empresas e na indústria de jogos. Python é essencial na evolução de sistemas de biotecnologia e computação 3D, áreas cruciais para o país. Grandes empresas como Zon, Globo, Air Canada e NASA utilizam Python em seus sistemas e projetos, mostrando sua versatilidade e importância. Além de empresas, a linguagem Python é amplamente empregada na indústria de jogos, com títulos populares como Civilization e Battlefield 2 sendo desenvolvidos com ela.
 
 O curso de Python é completo para iniciantes, sem pré-requisitos, abordando o funcionamento da linguagem e programação. O instrutor busca apoio para continuar produzindo conteúdo de qualidade. O instrutor destaca a dificuldade financeira em produzir cursos de alta qualidade sem apoio financeiro direto. Ele ressalta a importância da contribuição da comunidade para viabilizar o projeto. A qualidade do curso de Python é ressaltada, comparando com cursos anteriores. O instrutor busca apoio para manter e melhorar a qualidade do conteúdo oferecido aos alunos.
 """
