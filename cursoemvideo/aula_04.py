1b44e86d52a07e314587d8c6c4f3737b51d59e93
RepelloInimicum <dualsgo@icloud.com>
2024-06-03 21:54:31 -0300

    Curso Python #04 - Primeiros comandos em Python3

---
+++ cursoemvideo/aula_04.py
@@ -0,0 +1,31 @@
+# Curso Python #04 - Primeiros comandos em Python3
+
+"""
+Agora chegou a hora de aprender os comandos básicos do Python e fazer os primeiros programas em Linguagem Python.
+
+Nesta aula, vamos aprender a utilizar as instruções print() e input() para realizar interação com o usuário. Além disso, vamos ver o conceito fundamental de variável e como elas se comportam num programa em Python.
+
+Teremos exercícios resolvidos em Python, com desafios de programação, onde você terá que criar programas em Python de acordo com enunciados propostos durante a aula.
+
+
+Aprenda os primeiros comandos em Python3, como usar print, variáveis e input para interagir com o usuário. Crie scripts para executar tarefas básicas, como exibir mensagens e realizar cálculos simples.
+
+Momentos-chave:
+Aprender a interagir com o computador por comandos básicos de Python, como mostrar mensagens e realizar operações simples, é essencial para iniciantes em programação. Utilizar aspas para delimitar mensagens e não utilizá-las para números é uma prática fundamental. Explorando a importância dos operadores em Python e a necessidade de compreendê-los para avançar na programação. Destaque para a próxima aula sobre operadores e a base que estão sendo construídas. Explicando a utilização de aspas como delimitadores de mensagens em Python, seja com aspas simples ou duplas, e a distinção entre mensagens e números na linguagem de programação. Demonstrando a diferença na utilização de aspas para mensagens e a ausência delas para números, exemplificando a importância de compreender a distinção na programação.
+
+Ao utilizar o operador de soma em Python, é importante notar que ele pode juntar mensagens em vez de somar números, sendo necessária atenção ao uso de aspas. O vídeo também aborda a configuração do ambiente Idle no Windows para facilitar a prática de programação. A importância de utilizar aspas ao trabalhar com mensagens e números no Python para evitar erros de interpretação. Configuração do ambiente Idle no Windows para facilitar a prática de programação, tornando mais acessível o uso do Python de forma interativa.
+
+No Python, variáveis são objetos que podem receber valores, representados pelo sinal de igual. A interatividade com o usuário pode ser implementada utilizando a função input. A importância de compreender que variáveis no Python são objetos que podem armazenar valores específicos. Utilização do sinal de igual para atribuir valores às variáveis e a distinção entre igualdade e atribuição no Python. Implementação da interatividade com o usuário através da função input para coletar informações como nome, idade e peso.
+
+O vídeo demonstra como criar scripts em Python no modo interativo e como salvar e executar scripts separados, mostrando a transição entre os modos. A importância de testar e programar no modo interativo e de criar scripts para execução posterior é destacada. A importância do modo interativo para testar e experimentar comandos em Python antes de criar scripts separados. Demonstração de como salvar e executar scripts separados no IDLE, permitindo a execução de comandos sem interação direta.
+
+O vídeo apresenta desafios de programação em Python, incentivando os espectadores a praticarem e aprimorarem suas habilidades. Os desafios são progressivos e visam tornar a programação mais divertida e acessível. O primeiro desafio consiste em criar um script Python para exibir uma mensagem de boas-vindas personalizada. Os espectadores são encorajados a tentar resolver o desafio e compartilhar suas soluções. O segundo desafio envolve a formatação de uma mensagem com a data de nascimento de uma pessoa. Os espectadores são incentivados a praticar e a colaborar nos comentários. O terceiro desafio aborda a soma de dois números em Python, destacando a importância de aprender novas funções e buscar soluções. Os desafios são apresentados como etapas de aprendizado progressivo.
+
+O instrutor destaca a importância do apoio dos espectadores para a continuidade e aprimoramento do curso de Python, enfatizando a qualidade e diferenciais do conteúdo oferecido. Ele ressalta a necessidade de suporte financeiro para manter a estrutura e a produção do curso. O curso de Python oferece diversas metas, desde aulas e desafios até soluções e cursos avançados, prometendo um conteúdo completo e diferenciado. O instrutor destaca a qualidade do curso, com didática, animações e gamificação, ressaltando a diferença em relação a outros tutoriais disponíveis gratuitamente. O apoio financeiro dos espectadores é essencial para manter a qualidade e a continuidade do curso, devido aos custos envolvidos na produção e estrutura do conteúdo.
+"""
+
+# Desafio 01 - Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas conforme o valor digitado.
+
+# Desafio 02 - Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+# Desafio 03 - Crie um script Python que leia dois números e tente mostrar a soma entre eles.
